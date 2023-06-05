9f86987da2297e90a5e3af926aeb94cf6884baa9
glabojko <glabojko@gmail.com>
2023-06-05 23:09:23 +0200

    connection to database established1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import data_manager
 
 app = Flask(__name__)
@@ -10,7 +10,7 @@ def index():
 @app.route("/add_board")
 def add_board():
     data_manager.add_board_to_database("dupa")
-    return render_template('index.html')
+    return redirect('/')
 
 if __name__ == '__main__':
     app()
--- static/js/view/viewport.js
+++ static/js/view/viewport.js
@@ -2,7 +2,7 @@ const main_view = document.getElementById("root");
 
 let board = "<div class='bg-success class_board'>First board</div>";
 let button_add_board = "<button class='btn'>Add board</button>";
-// button_add_board.onclick = location.href='/add_board';
+button_add_board.onclick = location.href='/add_board';
 
 export let add_board = () => {
     main_view.innerHTML=board;
