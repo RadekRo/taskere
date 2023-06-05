4c96c01a093a21bdf0368af333b9f4c0c9dd97af
glabojko <glabojko@gmail.com>
2023-06-05 22:11:21 +0200

    first board added

---
+++ static/js/main.js
@@ -0,0 +1,4 @@
+import { add_board } from "./view/viewport.js";
+
+add_board()
+
---
+++ static/js/view/viewport.js
@@ -0,0 +1,5 @@
+const main_view = document.getElementById("root");
+
+let board = "<div>First board</div>";
+
+export let add_board = () => { main_view.innerHTML=board}; 
