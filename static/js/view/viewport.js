7cd870b6a4e4e23f74fa592aecc76bbb616eec25
RadekRo <r.roclawski@ankara.pl>
2023-06-06 08:03:24 +0200

    Adding one div to the page while adding board to base

--- static/js/view/viewport.js
+++ static/js/view/viewport.js
@@ -1,7 +1,7 @@
 export const main_view = document.getElementById("root");
 
 export let add_board = (title) => {
-    main_view.innerHTML+="<div class='bg-success'></div>";
+    main_view.innerHTML+="<div class='bg-success'>SUCCESS</div>";
     
 }
 
