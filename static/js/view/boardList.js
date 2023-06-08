67df69b8fdbe2acf2c8639753fe9a86bdba37bc1
RadekRo <r.roclawski@ankara.pl>
2023-06-08 13:42:56 +0200

    Created boardList function and Board class contructor

---
+++ static/js/view/boardList.js
@@ -0,0 +1,21 @@
+class Board {
+    constructor(boardId, textContent) {
+        this.board = document.createElement('div');
+        this.board.className = 'card card-body';
+        this.board.textContent = textContent;
+        this.board.setAttribute('id', boardId)
+    }
+    appendTo(parentElement) {
+        parentElement.appendChild(this.board)
+    }
+}
+
+export const boardList = () => {
+
+            let boardContainer = document.createElement('div');
+                boardContainer.setAttribute('id', 'board_list');
+                boardContainer.className = 'd-flex';
+}
+
+
+
