6b5c382dd7774af1a9441274cc9661092797818b
RadekRo <r.roclawski@ankara.pl>
2023-06-08 13:15:24 +0200

    Changed all functions to arrow functions and created new file boardList in view for creting list of currently created boards

--- static/js/view/boardForm.js
+++ static/js/view/boardForm.js
@@ -1,36 +1,36 @@
-export let boardForm = document.createElement('form');
+export const boardForm = document.createElement('form');
            boardForm.setAttribute('id', 'new_board');
            boardForm.method = 'POST';
            boardForm.action = '/add_board';
 
-export let boardNameLabel = document.createElement('label');
+export const boardNameLabel = document.createElement('label');
            boardNameLabel.htmlFor = 'board_title';
            boardNameLabel.textContent = 'Board title';
 
-export let boardNameInput = document.createElement('input');
+export const boardNameInput = document.createElement('input');
            boardNameInput.setAttribute('id', 'board_title');
            boardNameInput.name = 'title';
            boardNameInput.placeholder = 'Enter board title';
 
-export let boardNameSubmit = document.createElement('input');
+export const boardNameSubmit = document.createElement('input');
            boardNameSubmit.type = 'submit';
            boardNameSubmit.value = 'Save';
 
-let emptyInputAlert = document.createElement('div');
+const emptyInputAlert = document.createElement('div');
            emptyInputAlert.setAttribute('id', 'empty-input-alert');
            emptyInputAlert.className = 'bg-warning text=white'
            emptyInputAlert.innerHTML = 'Empty input field!'
 
 
-export let boardFormControl = () => {
-    document.getElementById("new_board").addEventListener("submit", function(event) {
+export const boardFormControl = () => {
+    document.getElementById("new_board").addEventListener("submit", (event) => {
         
         event.preventDefault();
       
         if (boardNameInput.value.trim() === '') {
             // alert('Please enter a board title.'); 
             boardForm.append(emptyInputAlert);
-            setTimeout(function() {
+            setTimeout(() => {
                 var div = document.getElementById('empty-input-alert');
                 div.parentNode.removeChild(div);
               }, 2000);
