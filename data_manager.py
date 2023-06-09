34e1706bf73921e33b9a7c1bebf57b20afd9a911
glabojko <glabojko@gmail.com>
2023-06-09 22:21:12 +0200

    date and time added to board creation

--- data_manager.py
+++ data_manager.py
@@ -1,16 +1,22 @@
 import database, random
+from datetime import datetime
 
 def get_random_number():
-    random_number = random.randint(1, 999999)
+    random_number = random.randint(1, 999999999)
     return random_number
 
+def get_current_date_time():
+    date_and_time = str(datetime.now())[0:19]
+    return date_and_time
+
 @database.connection_handler
-def add_board_to_database(cursor, title:str, random_number:int):
+def add_board_to_database(cursor, title:str, random_number:int, date_time:str):
     query = """
-    INSERT INTO boards (title, random_number)
-    VALUES (%(title)s, %(random_number)s)
+    INSERT INTO boards (title, random_number, addition_date_time)
+    VALUES (%(title)s, %(random_number)s, %(addition_date_time)s)
     """
-    data = {'title': title, 'random_number': random_number}
+    data = {'title': title, 'random_number': random_number, 
+            'addition_date_time': date_time}
     cursor.execute(query, data)
 
 
