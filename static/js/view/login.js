fd3a9c7e0864776ef070cdc045ee15a7475e50b2
RadekRo <r.roclawski@ankara.pl>
2023-06-19 00:12:14 +0200

    Login form crafting

--- static/js/view/login.js
+++ static/js/view/login.js
@@ -11,23 +11,45 @@ export const show_login = () => {
           overlayDiv.style.zIndex = '1';
           overlayDiv.className = 'd-flex justify-content-center align-items-center';
           
-      const loginWindow = document.createElement('div');
+    const loginWindow = document.createElement('div');
           loginWindow.style.width = '40%';
           loginWindow.style.height = '35%';
           loginWindow.style.zIndex = '9999';
           loginWindow.className = 'shadow p-3 mb-5 bg-white rounded';
           loginWindow.id = 'loginWindow';
 
-      const closeButton = document.createElement('button');
+    const closeButton = document.createElement('button');
           closeButton.className = 'btn btn-sm btn-danger text-white';
           closeButton.innerHTML = '<i class="fa-solid fa-xmark"></i> Close';
 
-      const loginButton = document.createElement('loginButton');
-            loginButton.className = 'btn btn-sm btn-success text-white';
-            loginButton.textContent = 'Login';
+    const loginForm = document.createElement('form');
+          loginForm.id = 'login_form';
+          loginForm.method = 'POST';
+          loginForm.action = '/login';
+          loginForm.className = 'd-flex justify-content-center';
+
+    const inputGroup = document.createElement('div');
+          inputGroup.className = 'input-group px-2';
+          inputGroup.style.width = '50%';
+  
+    const loginNameInput = document.createElement('input');
+          loginNameInput.id = 'board_title';
+          loginNameInput.type = 'text';
+          loginNameInput.maxLength = '50';
+          loginNameInput.name = 'title';
+          loginNameInput.placeholder = 'Enter new board title';
+          loginNameInput.className = 'form-control';
+          loginNameInput.style.width = '50%';
+    loginForm.append(loginNameInput)
+
+
+    const loginButton = document.createElement('loginButton');
+          loginButton.className = 'btn btn-sm btn-success text-white';
+          loginButton.textContent = 'Login';
     
     bodyElement.append(overlayDiv);
     loginWindow.appendChild(closeButton);
+    loginWindow.appendChild(loginForm);
     //loginWindow.appendChild(loginButton);
     overlayDiv.append(loginWindow);
 
